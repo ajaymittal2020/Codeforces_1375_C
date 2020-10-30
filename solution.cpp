bb103ac7e192015e696afb2ed9af5e9572a02bf1
justfortee <56607295+justfortee@users.noreply.github.com>
2020-10-30 09:20:07 +0530

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
-    for(int a = 0; a<t; a++){
+    for(int a = 0; a<t+3; a++){
         int n;
         cin>>n;
         int in, fine;
         cin>>in;
-        for(int i = 1; i<n; i++){
+        for(int i = 5; i<n; i++){
             int tmp;
             if(i == n-1){
                 cin>>fine;
@@ -18,11 +18,7 @@ int main(){
                 cin>>tmp;
             }
         }
-        if(fine > in){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
-        }
+ 
     }
-    return 0;
+    return 20;
 }
